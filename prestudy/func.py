005a41e12cc8b649719838488b5a46187c0284a0
17917 <1791745268@qq.com>
2021-04-21 14:55:12 +0800

    2021_4_21

--- prestudy/func.py
+++ prestudy/func.py
@@ -1,11 +1,12 @@
 # 时间：2021/4/18 18:22
-def fun1(a,b):
-    print(a,b)
-    return a+b
+def fun1(a, b):
+    print(a, b)
+    return a + b
 
-a=10
-b=20
-a1=0
-b1=0
-c=fun1(a,b)
+
+a = 10
+b = 20
+a1 = 0
+b1 = 0
+c = fun1(a, b)
 print(c)
